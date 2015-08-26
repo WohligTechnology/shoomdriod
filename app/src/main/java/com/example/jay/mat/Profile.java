02238ec5778826a569b747d75c6329ae9ba8ab8f
jayvisariya93 <jayvisariya93@gmail.com>
2015-08-26 16:48:16 +0530

    Revert "commit new for ui fixes"
    This reverts commit 8857e25c7838bb2a5f0a6c331de2b9e119302449.

--- app/src/main/java/com/example/jay/mat/Profile.java
+++ app/src/main/java/com/example/jay/mat/Profile.java
@@ -1,17 +1,13 @@
 package com.example.jay.mat;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -38,19 +34,6 @@ public class Profile extends ActionBarActivity implements NavigationDrawerCallba
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         tvHome = (TextView) findViewById(R.id.toolbar_title);
         setSupportActionBar(mToolbar);
-
-        ImageView iv= (ImageView) findViewById(R.id.imageView7);
-
-        iv.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Log.d("msg", "msg");
-                Intent search = new Intent(Profile.this, Search.class);
-                startActivity(search);
-
-            }
-        });
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
